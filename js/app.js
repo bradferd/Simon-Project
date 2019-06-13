46ae918f0c86a239d927b86290750727896c1ba5
Brad Bailey <bbailey921@gmail.com>
2019-06-13 09:36:50 -0400

    add event listeners to clickable areas

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+  console.log('ready!');
+  $('.square.blue').on('click', () => {
+    $('.square.blue').addClass('clicked');
+    setTimeout(function() {
+      $('.square.blue').removeClass('clicked');
+    }, 500);
+  });
+
+  $('.square.red').on('click', () => {
+    $('.square.red').addClass('clicked');
+    setTimeout(function() {
+      $('.square.red').removeClass('clicked');
+    }, 500);
+  });
+
+  $('.square.green').on('click', () => {
+    $('.square.green').addClass('clicked');
+    setTimeout(function() {
+      $('.square.green').removeClass('clicked');
+    }, 500);
+  });
+
+  $('.square.yellow').on('click', () => {
+    $('.square.yellow').addClass('clicked');
+    setTimeout(function() {
+      $('.square.yellow').removeClass('clicked');
+    }, 500);
+  });
+});
