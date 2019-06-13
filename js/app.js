7a77340ba29f3ec0e70035826e9a1efaa2605a9c
Brad Bailey <bbailey921@gmail.com>
2019-06-13 10:27:45 -0400

    add functionality to start game

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
   let isPlayerTurn = false;
+  let computerTurn = [];
+  let playerTurn = [];
+  let counter = 0;
+  let possibilities = ['#blue', '#red', '#green', '#yellow'];
 
-  console.log('ready!');
   $('.square.blue').on('click', () => {
     $('.square.blue').addClass('clicked');
     setTimeout(function() {
@@ -29,4 +32,39 @@ $(document).ready(function() {
       $('.square.yellow').removeClass('clicked');
     }, 500);
   });
+
+  let lightUp = function(square) {
+    console.log(square);
+  };
+
+  let updateCounter = function(counter) {
+    counter++;
+    $('h4.game-counter').text(`${counter}`);
+  };
+
+  let showComputerMove = function(sequence) {
+    var i = 0;
+    var interval = setInterval(function() {
+      lightUp(sequence[i]);
+      i++;
+      if (i >= sequence.length) {
+        clearInterval(interval);
+      }
+    }, 600);
+  };
+
+  let computerMove = function() {
+    computerTurn.push(possibilities[Math.floor(Math.random() * 4)]);
+    console.log(computerTurn);
+    showComputerMove(computerTurn);
+  };
+
+  let startGame = function() {
+    console.log('hello from Start Game');
+    counter = 0;
+    updateCounter(counter);
+    computerMove();
+  };
+
+  $('.btn-success').on('click', startGame);
 });
